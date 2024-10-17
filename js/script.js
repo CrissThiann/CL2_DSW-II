57a984146a4bc273b5967b483f28ce9155f64de4
Cristhian Fernando Vasquez Albornoz <cristhianalbornoz9@gmail.com>
2024-10-17 12:09:40 -0500

    3er Commit

--- js/script.js
+++ js/script.js
@@ -1,25 +1,34 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Selección de elementos
+    // Variables
     const btnRegistrarse = document.getElementById('btnRegistrarse');
     const btnIniciarSesion = document.getElementById('btnIniciarSesion');
-    const modalLogin = document.querySelector('.glass-container');
-    const modalRegister = document.querySelector('.glass-container-create');
+    const btnInicioSesion = document.getElementById('btnInicioSesion');
+    const modalLogin = document.getElementById('modalRegister');
+    const modalRegister = document.getElementById('modalCreate');
+    const mainContent = document.querySelector('.main-content');
 
-    // Asegurarse de que el formulario de Crear Cuenta esté oculto inicialmente
     modalRegister.style.display = 'none';
 
-    // Evento para mostrar el formulario de Crear Cuenta
+      // Evento para mostrar el formulario de Crear Cuenta
     btnRegistrarse.addEventListener('click', function(event) {
-        event.preventDefault(); // Evitar el comportamiento predeterminado del enlace
-        modalLogin.style.display = 'none';  // Ocultar el formulario de Login
-        modalRegister.style.display = 'block';  // Mostrar el formulario de Crear Cuenta
+        event.preventDefault();
+        modalLogin.style.display = 'none'; 
+        modalRegister.style.display = 'block'; 
     });
 
     // Evento para volver al formulario de Login
     btnIniciarSesion.addEventListener('click', function(event) {
-        event.preventDefault(); // Evitar el comportamiento predeterminado del enlace
-        modalRegister.style.display = 'none';  // Ocultar el formulario de Crear Cuenta
-        modalLogin.style.display = 'block';  // Mostrar el formulario de Login
+        event.preventDefault(); 
+        modalRegister.style.display = 'none';   
+        modalLogin.style.display = 'block';  
+    });
+
+    //Evento para mostrar el modal de Login y hacer el fondo transparente
+    btnInicioSesion.addEventListener('click', function(event){
+        event.preventDefault(); 
+        modalLogin.style.display = 'none';
+        modalRegister.style.display = 'block'; 
+        document.body.classList.add('transparent-background') 
     });
 });
 
